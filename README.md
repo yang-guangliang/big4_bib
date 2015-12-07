785ab8c071960eec696a68be628cdf0b6bb0380c
yang-guangliang <ygl@tamu.edu>
2015-12-07 08:07:21 -0600

    add ndss 2016

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 #### Statistics: How many papers are included ?
 
 
-| Conference     | Since(to now)  | Total Number | Accepted papers each year |  
-| :------------- | :---- | :----------- | :------------------------ |
-| Usenix Security| 2010- | 281 | 46/year |
-| CCS            | 2010- | 544 | 90/year |
-| NDSS           | 2009- | 270 | 38/year |
-| S&P            | 2009- | 267 | 38/year |
+| Conference     | From..to.. | Total Number | Accepted papers each year |  
+| :------------- | :-------- | :----------- | :------------------------ |
+| Usenix Security| 2010-2015 | 281 | 46/year |
+| CCS            | 2010-2015 | 544 | 90/year |
+| NDSS           | 2009-2016 | 333 | 41/year |
+| S&P            | 2009-2015 | 267 | 38/year |
---
+++ tools/gen_bib_from_paper_list.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# system modules
+import os
+import sys
+
+
+class Example(object):
+        def __init__(self):
+                pass
+
+prefix="""
+@article{helli,
+title={{
+"""
+surfix="""
+}},
+journal={ndss},
+year={2016},
+}
+"""
+
+def main():
+        if len(sys.argv) != 3:
+                print "Usage: \t" + sys.argv[0] + " <paper list>  <output file>"
+                sys.exit(-1)
+
+        PAPER_LIST = sys.argv[1]
+        OUTPUT_FILE = sys.argv[2]
+
+        fd = open(OUTPUT_FILE, "w+")
+        for line in open(PAPER_LIST):
+                line = line.strip()
+                line = prefix + line  + surfix
+                print line
+
+                fd.write(line)
+        fd.close()
+
+
+if __name__ == "__main__":
+        main()
