cc892bf16c4bbde0fb180c421fbeb7835772aab1
yang-guangliang <ygl@tamu.edu>
2016-05-27 16:26:08 -0500

    update security 16

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ##Introduction
-This repo is to share the list of accepted paper by top four computer security conferences (usenix security, ccs, ndss, sp) since 2009. You can easily import them to reference manager software.
+This repo is to share the list of accepted paper by the top-tier computer security conferences (Usenix Security, CCS, NDSS, S&P) since 2009. You can import them to any reference manager software to read it.
 
 --
 
@@ -8,7 +8,7 @@ This repo is to share the list of accepted paper by top four computer security c
 
 | Conference     | From..to.. | Total Number | Accepted papers each year |
 | :------------- | :-------- | :----------- | :------------------------ |
-| Usenix Security| 2010-2015 | 281 | 46/year |
+| Usenix Security| 2010-2016 | 351 | 50/year |
 | CCS            | 2010-2015 | 544 | 90/year |
 | NDSS           | 2009-2016 | 333 | 41/year |
 | S&P            | 2009-2016 | 322 | 40/year |
--- tools/gen_bib_from_paper_list.py
+++ tools/gen_bib_from_paper_list.py
@@ -3,22 +3,18 @@
 # system modules
 import os
 import sys
+import re
 
 
 class Example(object):
         def __init__(self):
                 pass
 
-prefix="""
-@article{,
-title={{
-"""
-surfix="""
-}},
-journal={s&p},
-year={2016},
-}
-"""
+FORMAT_STR = '@ARTICLE{,\n' + \
+             'author = "%s",\n' + \
+             'title = "%s",\n' + \
+             'journal = "NDSS",\n' + \
+             'year = "2016"\n}\n'
 
 def main():
         if len(sys.argv) != 3:
@@ -28,14 +24,36 @@ def main():
         PAPER_LIST = sys.argv[1]
         OUTPUT_FILE = sys.argv[2]
 
-        fd = open(OUTPUT_FILE, "w+")
-        for line in open(PAPER_LIST):
-                line = line.strip()
-                line = prefix + line  + surfix
-                print line
+        input_fd = open(PAPER_LIST)
+        output_fd = open(OUTPUT_FILE, "w+")
 
-                fd.write(line)
-        fd.close()
+        all_lines = input_fd.readlines()
+        i = 0
+        while i < len(all_lines):
+                title = all_lines[i].strip()
+                title = title.replace('"', '\'')
+                authors = all_lines[i + 1].strip()
+
+                # authors = filter(None, authors.split(","))
+                authors = [x.strip() for x in authors.split(",") if x.strip()]
+                new_authors_list = []
+
+                for each_author in authors:
+                        each_author_list = each_author.split()
+                        each_author_list = [x.strip() for x in each_author_list if x.strip()]
+                        if len(each_author_list) <= 1:
+                                raise Exception("each_author_list length is less than 1 : " + str(each_author_list))
+                        each_author_str = each_author_list[-1] + ", " + " ".join(each_author_list[:-1])
+                        new_authors_list.append(each_author_str)
+
+                print new_authors_list
+
+                output_fd.write(FORMAT_STR % (" and\n".join(new_authors_list), title))
+
+                i += 2
+
+        input_fd.close()
+        output_fd.close()
 
 
 if __name__ == "__main__":
